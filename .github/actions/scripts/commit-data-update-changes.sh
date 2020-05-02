273dd8e0340afeffce160e348ce0a69d137322f9
Rob Emanuele <rdemanuele@gmail.com>
2020-05-02 13:35:17 -0400

    Add action to make a pull request on new data update.

--- .github/actions/scripts/commit-data-update-changes.sh
+++ .github/actions/scripts/commit-data-update-changes.sh
@@ -6,16 +6,20 @@ if [[ -n "${COVID19_DEBUG}" ]]; then
     set -x
 fi
 
+TARGET_BRANCH=${1}
+
 PROCESSED_DATA_DIR=data/processed
 PUBLISHED_DATA_DIR=data/published
-NOTEBOOK_LOGS_DIR=notebook-logs
-TMP_SUFFIX="-AAA"
 
 function usage() {
 
     echo -n \
-        "Usage: $(basename "$0")
+        "Usage: $(basename "$0") CLONE_DIR
 Push data update changes as part of the data update workflow.
+
+Args:
+  CLONE_DIR: The directory of the local repository to work within.
+  TARGET_BRANCH: Target branch to check changes against, e.g. data/update
 "
 }
 
@@ -26,25 +30,46 @@ if [ "${BASH_SOURCE[0]}" = "${0}" ]; then
         git config --local user.email "action@github.com"
         git config --local user.name "GitHub Action"
 
-        # Only if there are diffs in the data update should we push.
-        if ! git diff --quiet ${PROCESSED_DATA_DIR} ${PUBLISHED_DATA_DIR}  && \
-                git diff --staged --quiet ${PROCESSED_DATA_DIR} ${PUBLISHED_DATA_DIR}; then
-            echo "DATA UPDATES FOUND."
+        git fetch origin
 
-            # Show what files changed
-            git status ${PROCESSED_DATA_DIR}
-            git status ${PUBLISHED_DATA_DIR}
+        # Show what files changed
+        git status ${PUBLISHED_DATA_DIR} ${PROCESSED_DATA_DIR}
+
+        if git diff --quiet -- ${PUBLISHED_DATA_DIR} ${PROCESSED_DATA_DIR}; then
+            echo "No changes to be committed to the repository."
+        else
+            echo "Changes found."
+        fi
+
+        if git ls-remote --exit-code origin ${TARGET_BRANCH}; then
+            echo "${TARGET_BRANCH} already exists."
+        else
+            echo "${TARGET_BRANCH} will be created if there are data changes."
+        fi
+
+        if git diff --quiet HEAD origin/${TARGET_BRANCH} -- ${PUBLISHED_DATA_DIR} ${PROCESSED_DATA_DIR}; then
+            echo "No differences found between HEAD and origin/${TARGET_BRANCH}"
+        else
+            git diff --compact-summary HEAD origin/${TARGET_BRANCH} -- ${PUBLISHED_DATA_DIR}
+        fi
+
+        # Only if there are diffs in the data update should we push.
+        if ! git diff --quiet -- ${PUBLISHED_DATA_DIR} ${PROCESSED_DATA_DIR}; then
 
             # Add our target changes and commit.
-            git add ${NOTEBOOK_LOGS_DIR}
-            git add ${PROCESSED_DATA_DIR}
-            git add ${PUBLISHED_DATA_DIR}
+            git add ${PUBLISHED_DATA_DIR} ${PROCESSED_DATA_DIR}
             git commit -m "Automatic data update for $(date -u +%Y-%m-%d)"
 
-            echo "::set-output name=status::CHANGED"
+            if ! git ls-remote --exit-code origin ${TARGET_BRANCH} || \
+                  ! git diff --quiet HEAD origin/${TARGET_BRANCH} -- ${PUBLISHED_DATA_DIR} ${PROCESSED_DATA_DIR}; then
+               echo "DATA UPDATES FOUND."
+               echo "::set-output name=status::CHANGED"
+           else
+               echo "DATA UPDATES ALREADY PUSHED."
+               echo "::set-output name=status::CHANGES_ALREADY_PUSHED"
+           fi
         else
             echo "NO DATA UPDATES FOUND."
-
             echo "::set-output name=status::NO_CHANGES"
         fi
     fi
