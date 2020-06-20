5ca2c11e3a5e96d17f226981da551742ecaac368
Rob Emanuele <rdemanuele@gmail.com>
2020-06-20 13:06:43 -0400

    Implement new GitHub action to update HSCAP data.

--- .github/actions/data-update-pr/index.js
+++ .github/actions/data-update-pr/index.js
@@ -7,23 +7,25 @@ async function run() {
     const token = core.getInput('token'),
           owner = core.getInput('owner') || github.context.repo.owner,
           repo = core.getInput('repo') || github.context.repo.repo,
-          title = core.getInput('title') || `Automatic data update for ${new Date().toISOString().substring(0,10)}`,
+          title_prefix = core.getInput('title') || 'Automatic data update',
+          title = title_prefix + ` - ${new Date().toISOString().substring(0,10)}`,
           base = core.getInput('base') || 'master',
           makeComment = core.getInput('makeComment') || 'true',
           comment = core.getInput('comment') || 'New data pushed!',
-          dataUpdateBranch = core.getInput('dataUpdateBranch') || owner + ':data/update';
+          dataUpdateBranch = core.getInput('dataUpdateBranch'),
+          dataUpdateBranchId = owner + ':' + dataUpdateBranch;
 
     const client = new github.GitHub(token);
 
-    console.log(`CHECKING PRs: ${owner}/${repo} ${dataUpdateBranch}`);
+    console.log(`CHECKING PRs: ${owner}/${repo} ${dataUpdateBranchId}`);
 
     const PRs = await client.pulls.list({
       owner: owner,
       repo: repo,
-      head: dataUpdateBranch,
+      head: dataUpdateBranchId,
       state: 'open'});
 
-    if(PRs.data.length > 0 && PRs.data[0].head.label == dataUpdateBranch) {
+    if(PRs.data.length > 0 && PRs.data[0].head.label == dataUpdateBranchId) {
       const issue_number = PRs.data[0].number;
 
       if(makeComment == 'true') {
@@ -40,13 +42,13 @@ async function run() {
         console.log(`SKIP COMMENTING ON PR #${issue_number}`);
       }
     } else {
-      console.log(`CREATING PR: ${owner}/${repo} HEAD ${dataUpdateBranch} BASE ${base}...`);
+      console.log(`CREATING PR: ${owner}/${repo} HEAD ${dataUpdateBranchId} BASE ${base}...`);
 
       await client.pulls.create({
         owner: owner,
         repo: repo,
         title: title,
-        head: dataUpdateBranch,
+        head: dataUpdateBranchId,
         base: base,
         maintainer_can_modify: true,
       });
